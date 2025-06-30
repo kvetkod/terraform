b7b5e26de3e95880f8c0b9bd9b0373b3608053e6
Alexey Golubev <alexey.golubev@itechart-group.com>
2025-06-30 19:18:25 +0300

    allowed egress for ecs

--- modules/ecs/ecs.tf
+++ modules/ecs/ecs.tf
@@ -91,10 +91,10 @@ resource "aws_security_group" "ecs_service" {
   vpc_id      = data.aws_vpc.default.id
 
   egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    security_groups = [var.rds_id]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
